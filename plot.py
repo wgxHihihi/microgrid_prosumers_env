137e0f82b49e8ab04c9100868490352cb3a5ef80
weiguixi <912782859@qq.com>
2022-10-28 17:08:54 +0800

    update plot.py

--- plot.py
+++ plot.py
@@ -16,7 +16,44 @@ def smooth(data, sm=1):
         y = np.ones(sm) * 1.0
         d = np.convolve(y, data, "same") / np.convolve(y, z, "same")
         smooth_data.append(d)
-    return smooth_data
+    return smooth_data[0]
+
+
+def get_mean_reward(paths, smooth_range):
+    df = {}
+    for algo, files in paths.items():
+        mean_r = []
+        for file in files:
+            data = pd.read_csv(file).round(2)
+            if smooth_range >= 3:
+                for key in data.keys():
+                    data[key] = smooth(data[key], smooth_range)
+            data = data.iloc[:, 1:]
+            data_np = data.values[:, :, np.newaxis]
+            mean_r.append(data_np)
+        mean_rewards = np.stack(mean_r, axis=-1)
+        mean_r_value = np.mean(mean_rewards, axis=-1)
+        df[algo] = mean_r_value
+    return df
+
+
+def plot_df(df_smooth: dict, df: dict, is_smooth: bool):
+    fig = plt.figure(figsize=(8, 5))
+    colors = {}
+    for algo, data in df_smooth.items():
+        total_r = data[:, 0, 0]
+        a = plt.plot(range(len(total_r)), list(total_r), label=algo)
+        colors[algo] = a[0]._color
+    if is_smooth:
+        for algo, data in df.items():
+            total_r = data[:, 0, 0]
+            # print(len(total_r))
+            plt.plot(range(len(total_r)), list(total_r), color=colors[algo], alpha=0.5)
+    plt.legend()
+    plt.xlim(0, 5000)
+    plt.ylabel('Rewards')
+    plt.xlabel('Episodes')
+    plt.show()
 
 
 def rewards_plot(paths):
@@ -60,6 +97,9 @@ if __name__ == '__main__':
                  'maddpg': project_dir + r'\train_logs\maddpg'
                  }
     ep = 5000
-
+    # colors = {'mappo': 'red', 'ippo': 'blue', 'maddpg': 'green'}
     file_paths = get_files_paths(logs_dirs, 'r_(.*){}\.csv'.format(ep))
-    rewards_plot(file_paths)
+    # rewards_plot(file_paths)
+    mean_r_smooth = get_mean_reward(file_paths, 31)
+    mean_r = get_mean_reward(file_paths, 0)
+    plot_df(mean_r_smooth, mean_r, True)
